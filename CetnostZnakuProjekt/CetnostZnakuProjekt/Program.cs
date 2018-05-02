3f758188d00813be51bbb7a6894c2266fa6e7b8c
Valouchj <38691196+Valouchj@users.noreply.github.com>
2018-05-02 17:41:04 +0200

    Osetreni vstupu

--- CetnostZnakuProjekt/CetnostZnakuProjekt/Program.cs
+++ CetnostZnakuProjekt/CetnostZnakuProjekt/Program.cs
@@ -27,124 +27,105 @@ namespace CetnostZnakuProjekt
                 sw.Dispose();
                 sw.Close();
             }
-            try
+
+            StreamReader reader = new StreamReader(fileName);
+
+            char ch = ' ';
+            string text = "";
+            int celkovyPocet = 0;
+            List<Pismeno> listP = new List<Pismeno>();
+
+            if (args.Length == 0)
             {
-                StreamReader reader = new StreamReader(fileName);
-
-
-
-                char ch = ' ';
-                string text = "";
-                int celkovyPocet = 0;
-                List<Pismeno> listP = new List<Pismeno>();
-
-                if (args.Length == 0)
+                Console.WriteLine("Nebyl zadán parametr, zadej text ke kontrole: (K ukončení pouzij # na konci)");
+                while (!text.EndsWith("#"))
                 {
-                    Console.WriteLine("Nebyl zadán parametr, zadej text ke kontrole: (K ukončení použij # na konci)");
-                    ch = (char)Console.Read();
-                    if (ch == '#')
-                    {
-                        Console.WriteLine("Nebyly zadány žádné znaky!?");
-                        Console.ReadKey();
-                        //Environment.Exit(0);
-                        return;
-                    }
-                    else { text += ch; }
-                    while (!text.EndsWith("#"))
-                    {
-                        text += Console.ReadLine();
-                    }
-
+                    text += Console.ReadLine();
                 }
-                text = text.ToLower();
+            }
+            text = text.ToLower();
 
-                int pozice = 0;
-                string temp = null;
-                bool run = true;
-                Pismeno d = new Pismeno(' ');
-                while (run)
+            int pozice = 0;
+            string temp = null;
+            bool run = true;
+            Pismeno d = new Pismeno(' ');
+            while (run)
+            {
+
+                if (args.Length > 0 || pozice == 0)
                 {
 
-                    if (args.Length > 0)
-                    {
-
-                        ch = (char)reader.Read();
-                        temp = ch.ToString();
-                        temp = temp.ToLower();
-                        ch = temp[0];
-                    }
-                    else
-                    {
-                        ch = text[pozice];
-                    }
-                    d.ch = ch;
-
-                    Pismeno result = listP.Find(x => x.ch == d.ch);
-
-
-                    if (result == null)
-                    {
-                        if ((int)ch >= 97 && (int)ch <= 122)
-                        {
-                            listP.Add(new Pismeno(ch, 1));
-                            celkovyPocet++;
-
-                        }
-
-                    }
-                    else
-                    {
-                        celkovyPocet++;
-                        listP.Find(x => x.ch == d.ch).pocet++;
-                    }
-
-
-                    if (reader.EndOfStream && args.Length > 0)
-                    {
-                        run = false;
-                    }
-
-                    if (ch == '#' && args.Length == 0)
-                    {
-                        run = false;
-                    }
-
-                    pozice++;
-                    if (ch != ' ' && ch != '#')
-                    {
-                        celkovypocetznaku++;
-                    }
-                }
-
-                if (listP.Count > 0)
-                {
-
-                    listP = listP.OrderBy(x => x.pocet).ToList();
-
-                    foreach (var item in listP)
-                    {
-
-                        Console.WriteLine($"Pismeno \"{item.ch}\" bylo pouzito {item.pocet}-krát, průměrná četnost znaků: {(item.pocet / (float)celkovyPocet) * 100} %");
-                    }
-                    int pocetZnakuVListu = listP.Count;
-                    Console.WriteLine("Celkový počet použítých znaků: {0}", celkovypocetznaku);
-                    Console.WriteLine($"Průměrné využití znaků z abecedy bylo: {pocetZnakuVListu / (float)PocetZnakuVAbecede * 100} %");
-                    Console.WriteLine($"Nejméně použitý znak: \"{listP[0].ch}\" s poctem znaku: {listP[0].pocet}");
-                    Console.WriteLine($"Nejvíce použitý znak: \"{listP[pocetZnakuVListu - 1].ch}\" s poctem znaku: {listP[pocetZnakuVListu - 1].pocet}");
+                    ch = (char)reader.Read();
+                    temp = ch.ToString();
+                    temp = temp.ToLower();
+                    ch = temp[0];
                 }
                 else
                 {
-                    Console.WriteLine("Nebyl použit žádný znak z abecedy");
+                    ch = text[pozice];
                 }
-                reader.Dispose();
-                reader.Close();
-                if (args.Length == 0) File.Delete(fileName);
+                d.ch = ch;
+
+                Pismeno result = listP.Find(x => x.ch == d.ch);
+
+
+                if (result == null)
+                {
+                    if ((int)ch >= 97 && (int)ch <= 122)
+                    {
+                        listP.Add(new Pismeno(ch, 1));
+                        celkovyPocet++;
+
+                    }
+
+                }
+                else
+                {
+                    celkovyPocet++;
+                    listP.Find(x => x.ch == d.ch).pocet++;
+                }
+
+
+                if (reader.EndOfStream && args.Length > 0)
+                {
+                    run = false;
+                }
+
+                if (ch == '#' && args.Length == 0)
+                {
+                    run = false;
+                }
+
+                pozice++;
+                if (ch != ' ' && ch != '#')
+                {
+                    celkovypocetznaku++;
+                }
             }
-            catch (Exception)
+
+            if (listP.Count > 0)
             {
-                Console.WriteLine($"Soubor neexistuje, zkontrolujce si nazev.");
-                Console.WriteLine($"Vas zadany nazev souboru{fileName}");
+
+                listP = listP.OrderBy(x => x.pocet).ToList();
+
+                foreach (var item in listP)
+                {
+
+                    Console.WriteLine($"Pismeno {item.ch} bylo pouzito {item.pocet} průměrná četnost znaku {(item.pocet / (float)celkovyPocet) * 100} %");
+                }
+                int pocetZnakuVListu = listP.Count;
+                Console.WriteLine("Celkový počet použítých znaků {0}", celkovypocetznaku);
+                Console.WriteLine($"Průměrné využití znaků z abecedy bylo: {pocetZnakuVListu / (float)PocetZnakuVAbecede * 100} %");
+                Console.WriteLine($"Nejméně použitý znak: {listP[0].ch} s poctem znaku : {listP[0].pocet}");
+                Console.WriteLine($"Nejvíce použitý znak: {listP[pocetZnakuVListu - 1].ch} s poctem znaku: {listP[pocetZnakuVListu - 1].pocet}");
             }
+            else
+            {
+                Console.WriteLine("Nebyl použit žádný znak z abecedy");
+            }
+            reader.Dispose();
+            reader.Close();
+            if (args.Length == 0) File.Delete(fileName);
 
             Console.ReadLine();
         }
